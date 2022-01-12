983d436187fcce7e363a23720aa7de4e2150f1fe
BohdanBesarab <BohdanBesarab>
2022-01-13 01:35:03 +0200

    add year month to version artifacts started do ansicolor without ending script

--- devAzureJenkins/vars/publishToAzureArtifacts.groovy
+++ devAzureJenkins/vars/publishToAzureArtifacts.groovy
@@ -1,3 +1,8 @@
+import java.time.*
+import java.time.format.DateTimeFormatter
+
+
 def call (String appName) {
-    sh "az artifacts universal publish --organization https://dev.azure.com/bohdanbesarab/ --project eleksCamp --scope project --feed jenkins --name ${appName} --version 1.0.${currentBuild.number} --path ${WORKSPACE}/app.zip"
+def tag = VersionNumber (versionNumberString: '${BUILD_DATE_FORMATTED, "yyyy.M"}.${BUILD_NUMBER}')
+    sh "az artifacts universal publish --organization https://dev.azure.com/bohdanbesarab/ --project eleksCamp --scope project --feed jenkins --name ${appName} --version ${tag} --path ${WORKSPACE}/app.zip"
 }
